b1985810481a1b2c5d2e5d9f84a618d0e60a3d23
RichardPalladino <rich@evolvedimprovement.com>
2023-03-11 16:10:44 -0800

    Ethernaut challenge #2 (fallout) complete with tests

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,5 +1,5 @@
 # Template to deploy solidity contract to a blockchain
-from brownie import network, config, accounts, Fallback
+from brownie import network, config, accounts, Fallback, Fallout
 
 LOCAL_BLOCKCHAINS = ["development", "ganache-local"]
 LOCAL_BLOCKCHAIN_FORKS = ["mainnet-fork", "mainnet-fork-dev"]
@@ -9,5 +9,13 @@ def deploy_fallback_contract() -> Fallback:
     account = accounts[0]
     print(f"Deploying fallback vulnerable contract from {account}....")
     contract = Fallback.deploy({"from": account})
+    print("Success! Fallback contract deployed at {}".format(contract.address))
+    return contract
+
+
+def deploy_fallout_contract() -> Fallout:
+    account = accounts[0]
+    print(f"Deploying fallback vulnerable contract from {account}....")
+    contract = Fallout.deploy({"from": account})
     print("Success! Fallback contract deployed at {}".format(contract.address))
     return contract
